1e083225dfc9ad20133be647c027a747fdbcb938
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-11-07 10:53:57 +0500

    Add files via upload

---
+++ Shengals_Roman_PR_33_zd_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Task2_Palindrome
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string word = Console.ReadLine();
+            word = word.ToUpper();
+            string rev = "";
+            for (int j = word.Length - 1; j >= 0; j--)
+                rev += word[j];
+            if (rev == word) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
