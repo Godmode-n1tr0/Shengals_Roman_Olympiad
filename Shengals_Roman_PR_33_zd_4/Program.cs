863ace877813cb5d0f3ac5504d7251fb69e733df
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-11-07 10:58:10 +0500

    Add files via upload

---
+++ Shengals_Roman_PR_33_zd_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Task4_Note
+{
+    static void Main()
+    {
+        Console.ReadLine(); 
+        string note = Console.ReadLine();
+        string paper = Console.ReadLine();
+
+        string[] noteWords = note.Split(' ');
+        string[] paperWords = paper.Split(' ');
+
+        for (int i = 0; i < noteWords.Length; i++)
+        {
+            bool found = false;
+            for (int j = 0; j < paperWords.Length; j++)
+            {
+                if (noteWords[i] == paperWords[j])
+                {
+                    paperWords[j] = ""; 
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine(noteWords[i]);
+                return;
+            }
+        }
+        Console.WriteLine("GOOD NOTE");
+    }
+}
