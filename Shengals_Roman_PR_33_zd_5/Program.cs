518c239727c59721c1ac7a6fa9c11ca2252668a5
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-11-07 10:59:11 +0500

    Add files via upload

---
+++ Shengals_Roman_PR_33_zd_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shengals_Roman_PR_33_zd_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
