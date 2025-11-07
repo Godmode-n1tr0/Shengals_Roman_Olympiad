20eb7b39a85695fa7efa3376ef705acf37a9cb5a
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-11-07 10:51:59 +0500

    Add files via upload

---
+++ Shengals_Roman_PR_33_zd_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Task1_Fish
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split();
+        int k = int.Parse(s[0]);
+        int n = int.Parse(s[1]);
+        Console.WriteLine(n / k);
+    }
+}
