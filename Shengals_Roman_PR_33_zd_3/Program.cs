cbd3711d9f198031708fa8f3b3f5b21c30da1740
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-11-07 10:56:13 +0500

    Add files via upload

---
+++ Shengals_Roman_PR_33_zd_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Task3_Snowmen
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] s = Console.ReadLine().Split();
+        long[] d = new long[2 * n];
+        for (int i = 0; i < 2 * n; i++) d[i] = long.Parse(s[i]);
+
+        Array.Sort(d); 
+        for (int i = 0; i < 2 * n; i += 2)
+        {
+            Console.WriteLine((i + 2) + " " + (i + 1)); 
+        }
+    }
+}
